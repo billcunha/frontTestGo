48c0ac329f7e1799dd501ec71c4ea92d8e47f503
billcunha <billcunha@gmail.com>
2017-10-19 23:31:44 -0200

    Bootstrap; List of scoreCards with filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+
+import { ScoreCardService } from './score-card.service';
+import { ScoreCard } from './score-card';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  public scoreCards : ScoreCard[];
+
+  constructor(
+    private ScoreCardService: ScoreCardService
+  ) {}
+
+  ngOnInit() {
+    this.updateScoreCards();
+  }
+
+  updateScoreCards (field ?: string, search ?: string){
+    let query : string;
+    if(field && search){
+      query = field + '=' + search;
+    }
+
+    this.ScoreCardService.getScoreCards(query)
+    .subscribe( result => {
+      this.scoreCards = result;
+    },
+    err => console.log(err)
+  );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
+import { Http, HttpModule } from '@angular/http';
+import { ScoreCardService } from './score-card.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
-import { IndexComponent } from './index/index.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IndexComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot(),
+    HttpModule
   ],
-  providers: [],
+  providers: [ScoreCardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
